46fe208a0c5fa9ec311b5a083563b0dbd85184bb
vitorlorente <lorente.sociologia@gmail.com>
2020-04-01 23:03:19 -0300

    Adicionando docstring à função de validação.

--- contato/utils.py
+++ contato/utils.py
@@ -1,4 +1,14 @@
 def validate_fields(contact):
+    """
+    Este método verifica se o json submetido para Contact possui todos os campos
+    exigidos pelo seu respectivo ContactFields.
+
+    Verifica também se um dos campos está vaio.
+    
+    TODO: No json de ContactFields deverá constar a informação da obrigatoriedade
+    do campo.
+    
+    """
     validation_keys = set(contact.fields_type.fields.keys())
     contact_keys = set(contact.contact_infos.keys())
     status = True
