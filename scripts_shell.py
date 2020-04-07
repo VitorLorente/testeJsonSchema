33869c50476a254f1641f2f2b5c4da4e44dbef7e
vitorlorente <lorente.sociologia@gmail.com>
2020-04-07 13:19:33 -0300

    Salvando scripts de teste no shell

---
+++ scripts_shell.py
@@ -0,0 +1,22 @@
+from contato.models import Contact
+from contato.utils import validate_fields
+contact = Contact.objects.all().select_related('fields_type').get()
+validate_fields(contact)
+
+from contato.contact_parser import ContactParser
+import os
+c = ContactParser(os.getcwd()+'/contacts_modelo1.txt', 2)
+c.go()
+
+from contato.contact_parser import ContactParser
+from contato.models import ContactFields
+import os
+path = os.getcwd() + '/contacts_modelo1.txt'
+pk_modelo = ContactFields.objects.first().pk
+parser = ContactParser(path, pk_modelo)
+parser.go()
+
+from contato.models import Contact
+from contato.utils import validate_fields
+c = Contact.objects.first()
+validate_fields(c)
